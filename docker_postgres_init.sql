40e834e0becd323f0f51342ac9ed2fc2de3cd03d
Alex Devero <deveroalex@gmail.com>
2022-05-10 17:45:05 +0200

    Update psql dump file

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,3 +1,12 @@
+CREATE DATABASE test_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 DROP TABLE IF EXISTS students;
 
 CREATE TABLE students (
